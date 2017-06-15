b3361eda2c7b51e392be8520e032995fc29ea570
Mark Vilrokx <mvilrokx@gmail.com>
2017-06-14 20:25:13 -0700

    Added sampleData - changed it slightly from what I got from Lucas to ease using it in the application, but data is the same

---
+++ src/sampleData.js
@@ -0,0 +1,9 @@
+const sampleData = [
+  'A tale about a kiwi drinking a late on the wiki.',
+  'No language like javascript.',
+  'Late to the party, Kile Kingdrin felt fashionable.',
+  'Sallie Rapty left early.',
+  'The allies entered the fray with Rafy Derente.',
+]
+
+export default sampleData
