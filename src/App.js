cf7011ed54eb0bdb25c08facc0bb754d45ddda79
Mark Vilrokx <mvilrokx@gmail.com>
2017-06-14 20:32:49 -0700

    First working version with a very simple version of a list of words, no functionality added other than that - I think this is as far as I got in the actual interview ;-)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import Anagrams from './Anagrams.js'
+import sampleData from './sampleData.js'
+
+const anagrams = new Anagrams()
 
 class App extends Component {
+
+  state = {
+    words: Array.prototype.concat(...anagrams.getAll()),
+  }
+
+  add = sentence => {
+    anagrams.document(sentence)
+    this.setState( {words: Array.prototype.concat(...anagrams.getAll())} )
+  }
+
+  /*
+   * Lifecycle Methods
+   */
+  componentWillMount() {
+    /* Normally this would be REST calls or DB calls, this is just for testing */
+    sampleData.forEach(sample => this.add(sample))
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className='clearfix p2'>
+        <div className="col-11 sm-col-11 md-col-6 lg-col-4 px2 mx-auto">
+          <ul>
+            {this.state.words.map(word => <li>{word}</li>)}
+          </ul>
+        </div>
       </div>
     );
   }
